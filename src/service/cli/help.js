498b8d263efddf82f50e1c2ca9fc34f06d9c8185
Alexander <al.deimosfrontend@gmail.com>
2020-11-17 20:39:11 +0300

    Учебный проект: асинхронность. Побег из ада коллбеков

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -1,10 +1,11 @@
 'use strict';
 
+const chalk = require(`chalk`);
 
 module.exports = {
   name: `--help`,
   run() {
-    console.info(`
+    console.info(chalk.gray(`
     Программа запускает http-сервер и формирует файл с данными для API.
 
     Гайд:
@@ -14,6 +15,7 @@ module.exports = {
     --version:            выводит номер версии
     --help:               печатает этот текст
     --generate <count>    формирует файл mocks.json
-    `);
+    `
+    ));
   }
 };
