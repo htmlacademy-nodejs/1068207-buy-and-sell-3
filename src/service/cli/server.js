f7c4363e7f0b93b10fd3b1748298a7bb7fdae651
Alexander <al.deimosfrontend@gmail.com>
2020-11-25 22:53:13 +0300

    Учебный проект: другой сервер

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,68 +1,23 @@
 'use strict';
 
-const chalk = require(`chalk`);
-const fs = require(`fs`).promises;
-const http = require(`http`);
-const PORT = 3000;
-const MOCK_FILE = `mocks.json`;
-const {HttpCode} = require(`../utils/_constants`);
+const express = require(`express`);
+const DEFAULT_PORT = 3000;
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
-
-const handleClientConnect = async (req, res) => {
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const rawOffers = await fs.readFile(MOCK_FILE);
-
-        const offerList = JSON.parse(rawOffers)
-          .map((offer) => `<li>${offer.title}</li>`)
-          .join(` `);
-
-        return sendResponse(res, HttpCode.OK, offerList);
-
-      } catch (error) {
-        console.error(chalk.red(error));
-        return sendResponse(res, HttpCode.NOT_FOUND, `Not Found`);
-      }
-
-    default: return sendResponse(res, HttpCode.NOT_FOUND, `Упс, такой страницы нет, 404`);
-  }
-
-};
-
-const handleServerConnect = async (error, port) => {
-  if (error) {
-    return console.error(chalk.red(`Ошибка при создании сервера, ${error}`));
-  }
-
-  return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-};
+const offersRouter = require(`../routes/offers`);
 
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
-    const port = Number(parseInt(customPort, 10)) || PORT;
+    const port = Number(parseInt(customPort, 10)) || DEFAULT_PORT;
+    const app = express();
+    app.use(express.json());
+    app.use(`/offers`, offersRouter);
 
-    http.createServer(handleClientConnect)
-      .listen(port)
-      .on(`listening`, (error) => handleServerConnect(error, port));
+    app.get(`/`, async (req, res) => {
+      return res.send(`Hello, Api Service!`);
+    });
+
+    app.listen(port, () => console.log(`Server has been started at ${port}`));
   }
 };
---
+++ src/service/routes/offers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fs = require(`fs`).promises;
+const {Router} = require(`express`);
+
+const MOCK_FILE = `mocks.json`;
+const {HttpCode} = require(`../utils/_constants`);
+
+const offersRouter = new Router();
+
+offersRouter.get(`/`, async (req, res) => {
+  try {
+    const result = await fs.readFile(MOCK_FILE);
+    return res.json(JSON.parse(result));
+  } catch (error) {
+    return res.status(HttpCode.INTERNAL_SERVER_ERROR).send(error);
+  }
+});
+
+module.exports = offersRouter;
